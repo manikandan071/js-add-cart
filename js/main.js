e0ed6ffcaacb9583d05a58811ebb060cc00120c2
manikandan071 <manidhiyamech07167@gmail.com>
2022-10-04 18:33:22 +0530

    js using add cart

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var product=[mobile=[{
+    id:1,product:"mobile",brand:"redmi",price:15000,qty:1
+},
+{
+    id:2,product:"headset",brand:"redmi",price:1000,qty:1
+},
+{
+    id:3,product:"powerbank",brand:"redmi",price:500,qty:1
+}],[
+    {
+        id:1,product:"mobile",brand:"redmi",price:15000,qty:1
+    },
+    {
+        id:2,product:"headset",brand:"redmi",price:1000,qty:1
+    },
+    {
+        id:3,product:"powerbank",brand:"redmi",price:500,qty:1
+    }
+]];
+
+console.log(product[0]);
